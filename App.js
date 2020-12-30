50ed9b23d24311ba863c68611ff22ef65e383a00
maryam banar <banar_maryam@yahoo.com>
2020-12-30 04:50:00 +0330

    Fifth-commit

--- App.js
+++ App.js
@@ -44,9 +44,17 @@ function App() {
 
   <div className="ENGLAND">
     <div className="ENGLAND-Box">
-      <div className="ENGLAND-Text">{currency.sana.data[1].updated_at}<p>یورو انگلیس</p></div> 
+      <div className="ENGLAND-Text">{currency.sana.data[11].updated_at}<p>پوند انگلیس</p></div> 
       <div className="ENGLAND-Sell"> {currency.sana.data[18].p}:فروش</div>
-      <div className="ENGLAND-Buy"> {currency.sana.data[1].p}:خرید</div>
+      <div className="ENGLAND-Buy"> {currency.sana.data[11].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="YORO">
+    <div className="YORO-Box">
+      <div className="YORO-Text">{currency.sana.data[11].updated_at}<p>یورو</p></div> 
+      <div className="YORO-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="YORO-Buy"> {currency.sana.data[11].p}:خرید</div>
     </div>
   </div>
 
@@ -55,6 +63,86 @@ function App() {
       <div className="CHINA-Text">{currency.sana.data[6].updated_at}<p>یوان چین</p></div> 
       <div className="CHINA-Sell"> {currency.sana.data[22].p}:فروش</div>
       <div className="CHINA-Buy"> {currency.sana.data[6].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="EMARAT">
+    <div className="EMARAT-Box">
+      <div className="EMARAT-Text">{currency.sana.data[2].updated_at}<p>درهم امارات</p></div> 
+      <div className="EMARAT-Sell"> {currency.sana.data[19].p}:فروش</div>
+      <div className="EMARAT-Buy"> {currency.sana.data[2].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="TURKI">
+    <div className="TURKI-Box">
+      <div className="TURKI-Text">{currency.sana.data[4].updated_at}<p>لیر ترکیه</p></div> 
+      <div className="TURKI-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="TURKI-Buy"> {currency.sana.data[4].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="KORIA">
+    <div className="KORIA-Box">
+      <div className="KORIA-Text">{currency.sana.data[7].updated_at}<p>وون کره جنوبی</p></div> 
+      <div className="KORIA-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="KORIA-Buy"> {currency.sana.data[7].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="SOEES">
+    <div className="SOEES-Box">
+      <div className="SOEES-Text">{currency.sana.data[8].updated_at}<p>فرانک سوییس</p></div> 
+      <div className="SOEES-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="SOEES-Buy"> {currency.sana.data[8].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="SOED">
+    <div className="SOED-Box">
+      <div className="SOED-Text">{currency.sana.data[12].updated_at}<p>کرون سويد</p></div> 
+      <div className="SOED-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="SOED-Buy"> {currency.sana.data[12].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="JAPEN">
+    <div className="JAPEN-Box">
+      <div className="JAPEN-Text">{currency.sana.data[9].updated_at}<p>ین ژاپن</p></div> 
+      <div className="JAPEN-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="JAPEN-Buy"> {currency.sana.data[9].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="NORVEZH">
+    <div className="NORVEZH-Box">
+      <div className="NORVEZH-Text">{currency.sana.data[13].updated_at}<p>کرون نروژ</p></div> 
+      <div className="NORVEZH-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="NORVEZH-Buy"> {currency.sana.data[13].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="ARAQ">
+    <div className="ARAQ-Box">
+      <div className="ARAQ-Text">{currency.sana.data[14].updated_at}<p>دینار عراق</p></div> 
+      <div className="ARAQ-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="ARAQ-Buy"> {currency.sana.data[14].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="AUS">
+    <div className="AUS-Box">
+      <div className="AUS-Text">{currency.sana.data[15].updated_at}<p>دلار استرالیا</p></div> 
+      <div className="AUS-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="AUS-Buy"> {currency.sana.data[15].p}:خرید</div>
+    </div>
+  </div>
+
+  <div className="KANADA">
+    <div className="KANADA-Box">
+      <div className="KANADA-Text">{currency.sana.data[10].updated_at}<p>دلار کانادا</p></div> 
+      <div className="KANADA-Sell"> {currency.sana.data[18].p}:فروش</div>
+      <div className="KANADA-Buy"> {currency.sana.data[10].p}:خرید</div>
     </div>
   </div>
   </div>
